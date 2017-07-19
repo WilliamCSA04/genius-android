17464693137fc86ff078a3e66044d7107ac92c59
williamcsa04 <williamcaetano14@gmail.com>
2017-07-18 21:00:56 -0300

    Created button for tests

--- app/src/main/java/com/example/realnetwoking/genius_android/Main.java
+++ app/src/main/java/com/example/realnetwoking/genius_android/Main.java
@@ -1,15 +1,37 @@
 package com.example.realnetwoking.genius_android;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import static android.R.attr.button;
 
 public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView view = new TextView(this);
-        view.setText("hello world");
-        setContentView(view);
+        setContentView(R.layout.layout);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        Button button = (Button) findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                //Create code for button
+
+            }
+
+        });
 
     }
 }
