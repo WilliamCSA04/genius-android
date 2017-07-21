c608705c90565ba8e8cb0317936f151ecfc100a8
williamcsa04 <williamcaetano14@gmail.com>
2017-07-20 21:03:54 -0300

    Created alert to start button

--- app/src/main/java/com/example/realnetwoking/genius_android/Main.java
+++ app/src/main/java/com/example/realnetwoking/genius_android/Main.java
@@ -1,12 +1,15 @@
 package com.example.realnetwoking.genius_android;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.R.attr.button;
 
@@ -27,8 +30,10 @@ public class Main extends Activity {
 
             @Override
             public void onClick(View arg0) {
-                //Create code for button
-
+                AlertDialog alert = new AlertDialog.Builder(Main.this).create();
+                alert.setTitle("Not implemented yet");
+                alert.setMessage("The game activity is being developed");
+                alert.show();
             }
 
         });
